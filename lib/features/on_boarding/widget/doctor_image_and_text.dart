2f58f4f210cdede17aeda4795c30a2c1b0c75d15
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-03 16:22:35 +0300

    added onboarding wigdets

---
+++ lib/features/on_boarding/widget/doctor_image_and_text.dart
@@ -0,0 +1,40 @@
+import 'package:doctors_app/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DoctorImageAndText extends StatelessWidget {
+  const DoctorImageAndText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      // alignment: Alignment.topCenter,
+      children: [
+        SvgPicture.asset('assets/svgs/doc_logo_low_opacity.svg'),
+        Container(
+          foregroundDecoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [
+                    Colors.white,
+                    Colors.white.withOpacity(0.0),
+                  ],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  stops: [0.14, 0.4])),
+          child: Image(image: AssetImage('assets/svgs/onboarding_doctor.png')),
+        ),
+        Positioned(
+            bottom: 30,
+            left: 30,
+            right: 30,
+            child: Text(
+              'Best Doctor Appointment App',
+              textAlign: TextAlign.center,
+              style: TextStyles.font32Blue700Weight.copyWith(
+                height: 1.4
+              ),
+            ))
+      ],
+    );
+  }
+}
