50df9ad4442237ae5b496e94edfc8497c32ea5fe
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-03 16:23:08 +0300

    added onboarding wigdets

--- lib/features/on_boarding/widget/doc_logo_widget.dart
+++ lib/features/on_boarding/widget/doc_logo_widget.dart
@@ -1,5 +1,3 @@
-
-import 'package:doctors_app/core/theming/colors.dart';
 import 'package:doctors_app/core/theming/styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
