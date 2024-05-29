2422dd34c64e794e1d0526210f26261e8deb885a
ranamohs <ranamohsen868_sd@fcis.bsu.edu.eg>
2024-05-29 19:14:10 +0300

    SignUp Ui & Logic Created

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,14 +1,17 @@
 import 'package:doctors_app/core/di/dependancy_injection.dart';
 import 'package:doctors_app/core/routing/routes.dart';
-import 'package:doctors_app/features/home/ui/screens/home_screen.dart';
 import 'package:doctors_app/features/login/logic/login_cubit.dart';
-import 'package:doctors_app/features/login/ui/screens/login_screen.dart';
 import 'package:doctors_app/features/on_boarding/screen/on_boarding_screen.dart';
+import 'package:doctors_app/features/sign_up/ui/screens/sign_up_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../features/home/ui/screens/home_screen.dart';
+import '../../features/login/ui/screens/login_screen.dart';
+import '../../features/sign_up/logic/sign_up_cubit.dart';
+
 
 class AppRouter {
-  static Route generateRoute(RouteSettings settings) {
+  Route generateRoute(RouteSettings settings) {
     //this arguments to be passed in any screen like this ( arguments as ClassName )
     final arguments = settings.arguments;
 
@@ -17,20 +20,24 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => const OnBoardingScreen(),
         );
-
       case Routes.loginScreen:
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (context) => getIt<LoginCubit>(),
-              child: const LoginScreen()),
+            child: const LoginScreen(),
+          ),
         );
-
+      case Routes.signUpScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<SignupCubit>(),
+            child: const SignupScreen(),
+          ),
+        );
       case Routes.homeScreen:
         return MaterialPageRoute(
           builder: (_) => const HomeScreen(),
         );
-
-
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(
